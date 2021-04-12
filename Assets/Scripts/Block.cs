6c63f7ab74020e54e352244433c7b10b25b681e7
Unknown <aznnoodle101@hotmail.com>
2021-04-12 02:34:20 -0700

    Basic PC Player Controls, Basic Raycasting
    * added basic player controls on PC keyboard and mouse (as opposed to VR)
    * added basic raycasting that detects a Block that is a Structure
    * added a base class of Structure for crafting and building, with an inheriting child class of Block

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : Structure
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this.type = Structure.Type.Block;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
