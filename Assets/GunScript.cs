b8f49a362a6638ff0c6bf901e4f1872fc63aeb54
Kristian De Castro <kdec001@ucr.edu>
2021-04-16 16:22:19 -0700

    Modified Gun Properties
    * Added basic hit impact effect
    * Added fire rate
    * Commented out debug logs

--- Assets/GunScript.cs
+++ Assets/GunScript.cs
@@ -4,12 +4,26 @@ public class GunScript : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 15f;
+    public float impactForce = 60f;
+
+    public GameObject impactEffect;
     public Camera fpsCam;
+
+    private float nextTTF = 0f;
+
+    CursorLockMode lockMode;
+
+    void Awake(){
+        lockMode = CursorLockMode.Locked;
+        Cursor.lockState = lockMode;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1")){
+        if(Input.GetButton("Fire1") && Time.time >= nextTTF){
+            nextTTF = Time.time + 1f/fireRate; 
             Shoot();
         }
     }
@@ -23,6 +37,14 @@ public class GunScript : MonoBehaviour
             if(tgt != null){
                 tgt.damageTake(damage);
             }
+
+            GameObject impactObject = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactObject, 1.5f);
         }
+
+        if(hit.rigidbody != null){
+            hit.rigidbody.AddForce(-hit.normal*impactForce);
+        }
+
     }
 }
