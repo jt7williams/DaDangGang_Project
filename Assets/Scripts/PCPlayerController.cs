9961735f81e6731a7d5601bedb1d555477351152
Kristian De Castro <kdec001@ucr.edu>
2021-04-23 16:50:55 -0700

    Modified Gun Script
    * Added bullet velocity
    * Added bullet drop
    * Added Tracer effect
    * Improved Hit impact effect
    * Imported basic unity particle effects
    * Added more modifiable options to guns
    * Removed spawning hit impacts lessening space used

--- Assets/Scripts/PCPlayerController.cs
+++ Assets/Scripts/PCPlayerController.cs
@@ -4,34 +4,60 @@ using UnityEngine;
 
 public class PCPlayerController : MonoBehaviour
 {
-    public new Camera camera;
+    public Camera mainCamera;
     public GameObject body;
     public Vector3 movement;
     public Vector3 rotation;
 
+    public Transform player;
+    GunScript weapon;
+
+
     void Start()
     {
-        
+        //mainCamera = Camera.main;
+        weapon = GetComponentInChildren<GunScript>();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update(){
         movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * 0.1f;
         rotation = new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * 3;
+        
     }
 
     private void FixedUpdate(){
-        Debug.DrawLine(this.transform.position, this.transform.position + this.camera.transform.forward * 3, Color.magenta);
+        Debug.DrawLine(this.transform.position, this.transform.position + this.mainCamera.transform.forward * 3, Color.magenta);
         Debug.DrawLine(this.body.transform.position, this.body.transform.position + this.body.transform.forward * 3, Color.yellow);
 
-        Vector3 cameraDirection = this.camera.transform.rotation.eulerAngles;
+        Vector3 cameraDirection = this.mainCamera.transform.rotation.eulerAngles;
         this.body.transform.rotation = Quaternion.Euler(new Vector3(0, cameraDirection.y, cameraDirection.z));
-        
-        movement = this.camera.transform.TransformDirection(movement);
+
+        movement = this.mainCamera.transform.TransformDirection(movement);
         movement.y = 0;
         this.transform.position += movement;
 
-        this.camera.transform.eulerAngles += rotation;
-        
+        this.mainCamera.transform.eulerAngles += rotation;
+
     }
+
+
+    //private void LateUpdate()
+    //{
+    //    if (Input.GetButton("Fire1"))
+    //    {
+    //        weapon.StartFiring();
+    //    }
+    //    if (weapon.isFiring)
+    //    {
+    //        weapon.UpdateFiring(Time.deltaTime);
+    //    }
+    //    weapon.UpdateBullets(Time.deltaTime);
+    //    if (Input.GetButtonUp("Fire1"))
+    //    {
+    //        weapon.StopFiring();
+    //    }
+    //}
 }
